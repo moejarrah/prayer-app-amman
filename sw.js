cda7c14a13edd5c197605da93ad0515898ddbffe
Mohammad Aljarrah <mohammadaljarrah@live.com>
2026-06-01 00:57:50 +0300

    Manual updates: icons, themes preview, app tweaks

--- sw.js
+++ sw.js
@@ -1,27 +1,53 @@
-const CACHE = "prayer-times-v3";
-const FILES = ["./", "./index.html", "./sw.js", "./manifest.json", "./prayers-data.js"];
+const CACHE = "prayer-times-v4";
+const STATIC_FILES = ["./", "./index.html", "./manifest.json", "./prayers-data.js", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 
 self.addEventListener("install", e => {
   self.skipWaiting();
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(FILES)));
+  e.waitUntil(
+    caches.open(CACHE).then(c => c.addAll(STATIC_FILES))
+  );
 });
 
 self.addEventListener("activate", e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-    )
+    ).then(() => self.clients.claim())
   );
 });
 
 self.addEventListener("fetch", e => {
-  e.respondWith(
-    fetch(e.request)
-      .then(res => {
-        const clone = res.clone();
-        caches.open(CACHE).then(c => c.put(e.request, clone));
-        return res;
+  const url = new URL(e.request.url);
+
+  if (e.request.mode === "navigate") {
+    e.respondWith(
+      caches.match("./index.html").then(cached => {
+        const networkFetch = fetch(e.request).then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put("./index.html", clone));
+          return res;
+        }).catch(() => cached);
+        return networkFetch;
       })
-      .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  if (STATIC_FILES.some(f => url.pathname.endsWith(f.replace("./", "")))) {
+    e.respondWith(
+      caches.match(e.request).then(cached => {
+        const networkFetch = fetch(e.request).then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        }).catch(() => cached);
+        return cached || networkFetch;
+      })
+    );
+    return;
+  }
+
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
   );
 });
