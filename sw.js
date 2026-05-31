ac1efeadde6c773a1b4659cfa85d206b8d515164
Mohammad Aljarrah <mohammadaljarrah@live.com>
2026-06-01 02:55:31 +0300

    Fix bottom gap: status-bar black, ::after strip, fixed nav back, nav+app bg vars, sw auto-update+reload v8

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "prayer-times-v6";
+const CACHE = "prayer-times-v8";
 const STATIC_FILES = ["./", "./index.html", "./manifest.json", "./prayers-data.js", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 const STATIC_PATHS = new Set(
   STATIC_FILES
